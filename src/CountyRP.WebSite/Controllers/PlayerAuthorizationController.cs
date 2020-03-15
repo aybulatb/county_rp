23049535b0c0f15bb061caaaa95a43304347cd4a
Третьяков Илья <kshaddix1413@gmail.com>
2020-03-15 21:33:43 +0300

    Work with auth website

---
+++ src/CountyRP.WebSite/Controllers/PlayerAuthorizationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using CountyRP.Extra;
+
+namespace CountyRP.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerAuthorizationController : ControllerBase
+    {
+        private PlayerAuthorizationClient _playerAuthorizationClient;
+
+        public PlayerAuthorizationController(PlayerAuthorizationClient playerAuthorizationClient)
+        {
+            _playerAuthorizationClient = playerAuthorizationClient;
+        }
+
+        [HttpGet]
+        [Route("tryauthorize")]
+        public async Task<IActionResult> TryAuthorize(string login, string password)
+        {
+            try
+            {
+                var x = await _playerAuthorizationClient.TryAuthorizeAsync(login, password);
+
+                return Content($"{x.Id} {x.Login} {x.Password}");
+            }
+            catch (ApiException ex)
+            {
+                return Content($"{ex.StatusCode}");
+            }
+        }
+    }
+}
