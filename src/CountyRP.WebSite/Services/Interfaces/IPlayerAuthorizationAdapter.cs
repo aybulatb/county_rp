40b4098ba2dee20ed5971bb7ce9f23aedaada48d
Третьяков Илья <kshaddix1413@gmail.com>
2020-03-15 21:33:43 +0300

    Work with auth website

---
+++ src/CountyRP.WebSite/Services/Interfaces/IPlayerAuthorizationAdapter.cs
@@ -0,0 +1,7 @@
+﻿namespace CountyRP.WebSite.Services.Interfaces
+{
+    public interface IPlayerAuthorizationAdapter
+    {
+        void TryAuthorize(string login, string password);
+    }
+}
--- src/CountyRP.WebSite/Startup.cs
+++ src/CountyRP.WebSite/Startup.cs
@@ -1,10 +1,12 @@
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+using CountyRP.Extra;
 
 namespace CountyRP.WebSite
 {
@@ -28,6 +30,9 @@ namespace CountyRP.WebSite
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            HttpClient httpClient = new HttpClient();
+            services.AddSingleton(new PlayerAuthorizationClient(httpClient));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
