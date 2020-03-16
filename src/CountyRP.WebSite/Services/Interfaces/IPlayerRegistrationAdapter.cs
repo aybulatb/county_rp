113c33c2bb4e0df9212173b36542b533168a6405
Третьяков Илья <kshaddix1413@gmail.com>
2020-03-16 23:35:49 +0300

    Add registration for business logic and website parts

---
+++ src/CountyRP.WebSite/Services/Interfaces/IPlayerRegistrationAdapter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CountyRP.WebSite.Services.Interfaces
+{
+    public interface IPlayerRegistrationAdapter
+    {
+        Task Register(string login, string password);
+    }
+}
