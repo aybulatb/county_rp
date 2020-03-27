9a6b7426a239c05d827c655439cbf8b9c6ee6fce
KShaddix <kshaddix1413@gmail.com>
2020-03-27 23:24:47 +0300

    Add Player and Person getter from WebAPI (#2)

---
+++ src/CountyRP.WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CountyRP.Entities;
+using CountyRP.WebAPI.Models;
+
+namespace CountyRP.WebAPI.Controllers
+{
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private PlayerContext _playerContext;
+
+        public PersonController(PlayerContext playerContext)
+        {
+            _playerContext = playerContext;
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public Person GetById(int id)
+        {
+            return _playerContext.Persons.FirstOrDefault(p => p.Id == id);
+        }
+
+        [HttpGet]
+        [Route("GetByName")]
+        public Person GetByName(string name)
+        {
+            return _playerContext.Persons.FirstOrDefault(p => p.Name == name);
+        }
+    }
+}
---
+++ src/CountyRP.WebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CountyRP.Entities;
+using CountyRP.WebAPI.Models;
+
+namespace CountyRP.WebAPI.Controllers
+{
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private PlayerContext _playerContext;
+
+        public PlayerController(PlayerContext playerContext)
+        {
+            _playerContext = playerContext;
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public Player GetById(int id)
+        {
+            return _playerContext.Players.FirstOrDefault(p => p.Id == id);
+        }
+
+        [HttpGet]
+        [Route("GetByLogin")]
+        public Player GetByLogin(string login)
+        {
+            return _playerContext.Players.FirstOrDefault(p => p.Login == login);
+        }
+    }
+}
