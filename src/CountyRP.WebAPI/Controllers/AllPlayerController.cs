c90c08597ca0e1295c7a44442edc8fc6f8da59b4
Третьяков Илья <kshaddix1413@gmail.com>
2020-04-06 01:10:54 +0300

    Add AllPlayer entity to WebAPI

---
+++ src/CountyRP.WebAPI/Controllers/AllPlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using CountyRP.Entities;
+using CountyRP.WebAPI.Models;
+using CountyRP.WebAPI.Models.ViewModels;
+
+namespace CountyRP.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AllPlayerController : ControllerBase
+    {
+        private PlayerContext _playerContext;
+
+        public AllPlayerController(PlayerContext playerContext)
+        {
+            _playerContext = playerContext;
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        [ProducesResponseType(typeof(AllPlayer), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public IActionResult GetById(int id)
+        {
+            Player player = _playerContext.Players.FirstOrDefault(p => p.Id == id);
+            if (player == null)
+                return BadRequest();
+
+            List<Person> persons = _playerContext.Persons.Where(p => p.PlayerId == player.Id).ToList();
+
+            AllPlayer allPlayer = new AllPlayer
+            {
+                Player = player,
+                Persons = persons
+            };
+            return Ok(allPlayer);
+        }
+
+        [HttpGet]
+        [Route("GetByLogin")]
+        [ProducesResponseType(typeof(AllPlayer), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public IActionResult GetByLogin(string login)
+        {
+            Player player = _playerContext.Players.FirstOrDefault(p => p.Login == login);
+            if (player == null)
+                return BadRequest();
+
+            List<Person> persons = _playerContext.Persons.Where(p => p.PlayerId == player.Id).ToList();
+
+            AllPlayer allPlayer = new AllPlayer
+            {
+                Player = player,
+                Persons = persons
+            };
+            return Ok(allPlayer);
+        }
+    }
+}
---
+++ src/CountyRP.WebAPI/Models/ViewModels/AllPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using CountyRP.Entities;
+
+namespace CountyRP.WebAPI.Models.ViewModels
+{
+    public class AllPlayer
+    {
+        public Player Player { get; set; }
+        public List<Person> Persons { get; set; }
+    }
+}
