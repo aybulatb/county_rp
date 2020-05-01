1345645feccefaa318aaf2d3b407c36e03a7c8be
Третьяков Илья <kshaddix1413@gmail.com>
2020-05-01 20:05:19 +0300

    Add House entity and actions

--- src/CountyRP.WebAPI/Models/PropertyContext.cs
+++ src/CountyRP.WebAPI/Models/PropertyContext.cs
@@ -7,6 +7,7 @@ namespace CountyRP.WebAPI.Models
     public class PropertyContext : DbContext
     {
         public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<House> Houses { get; set; }
 
         public PropertyContext(DbContextOptions<PropertyContext> options)
             : base(options)
